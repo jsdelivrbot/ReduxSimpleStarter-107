798b350ac94d132cc45164165ddf9152a568b00a
Dan Amyot <amyot.dan@gmail.com>
2018-08-30 13:33:13 -0400

    Current progress to pick up at work

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import * as config from './config';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = config.keys.youtube;
 
-// Create a new component, it will produce HTML
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+	console.log(data);
+});
+
+// Create a new component that renders HTML
 const App = () => {
-  return <div>Hi!</div>;
-}
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
+};
 
 // Take this component's genereated HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
